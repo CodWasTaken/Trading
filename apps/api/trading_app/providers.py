f14c26a530167d5d0101919518da012da8d0bd8a
Cod <Cod3eater@gmail.com>
2026-07-28 21:16:32 +0200

    Use entity catalog in live news enrichment

--- apps/api/trading_app/providers.py
+++ apps/api/trading_app/providers.py
@@ -12,6 +12,7 @@ import websockets
 
 from .config import Settings
 from .domain import NewsEvent, Quote
+from .entities import EntityCatalog
 from .news_intelligence import NewsIntelligence
 
 
@@ -79,7 +80,9 @@ class AlpacaWebSocketFeed:
     def __init__(self, settings: Settings) -> None:
         self.settings = settings
         self.key, self.secret = settings.require_alpaca_credentials()
-        self.news_intelligence = NewsIntelligence()
+        self.news_intelligence = NewsIntelligence(
+            entity_catalog=EntityCatalog.load(settings.trading_entity_catalog_path)
+        )
 
     async def _authenticate(self, socket: Any) -> None:
         await socket.send(
